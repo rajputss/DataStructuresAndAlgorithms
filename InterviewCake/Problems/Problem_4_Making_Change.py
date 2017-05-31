3e5b0ee5faa6e3586392cac8852a5ba07fe394ce
Shawn Rajput <shawn.saeed01@gmail.com>
2017-05-30 19:24:47 -0500

    Added solution to Problem 2 - calculating product of all numbers except the index.

---
+++ InterviewCake/Problems/Problem_4_Making_Change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+def change_possibilities_bottom_up(amount, denominations):
+    ways_of_doing_n_cents = [0] * (amount + 1)
+    ways_of_doing_n_cents[0] = 1
+
+    for coin in denominations:
+        for higher_amount in range(coin, amount + 1):
+            higher_amount_remainder = higher_amount - coin
+            ways_of_doing_n_cents[higher_amount] += ways_of_doing_n_cents[higher_amount_remainder]
+    return ways_of_doing_n_cents[amount]
+
+
+a = 5
+d = [1, 3, 5]
+
+print(change_possibilities_bottom_up(a, d))

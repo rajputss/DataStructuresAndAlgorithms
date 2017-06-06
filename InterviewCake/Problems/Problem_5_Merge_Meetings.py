d2e298657fad0da882f12e722ff0fe3ce36bc235
Shawn Rajput <shawn.saeed01@gmail.com>
2017-06-05 20:20:44 -0500

    Modified Problem 5, added comments explaining the code

--- InterviewCake/Problems/Problem_5_Merge_Meetings.py
+++ InterviewCake/Problems/Problem_5_Merge_Meetings.py
@@ -7,13 +7,16 @@ def merge_ranges(meetings):
 
     # initialize merged_meetings with the earliest meeting
     merged_meetings = [sorted_meetings[0]]
-
+    # get current meeting start and end tuple from sorted meeting
     for current_meeting_start, current_meeting_end in sorted_meetings [1:]:
+        # assign values from merged meetings to last merged meeting start and end
         last_merged_meeting_start, last_merged_meeting_end = merged_meetings[-1]
 
         # if the current and last meetings overlap, use the latest end time
+        # to assign to merged meetings
         if current_meeting_start <= last_merged_meeting_end:
-            merged_meetings[-1] = (last_merged_meeting_start, max(last_merged_meeting_end, current_meeting_end))
+            merged_meetings[-1] = (last_merged_meeting_start,
+                                   max(last_merged_meeting_end, current_meeting_end))
 
         # add the current meeting since it doesn't overlap
         else:
